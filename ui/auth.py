9cf97f482d21bc1a3ab8adb5c4c382594964f371
ExoSkeleton <calazulazu@gmail.com>
2025-12-15 11:19:17 +0200

    browse online terms

--- ui/auth.py
+++ ui/auth.py
@@ -1,4 +1,5 @@
 import sys
+import webbrowser
 from tkinter import ttk
 from tkinter.font import Font
 from typing import Callable, Any
@@ -17,10 +18,14 @@ class TermsPage(WidgetFrame):
         # terms
         ttk.Label(self, text="Consent Form", font=Font(size=12, weight="bold")).pack()
         ttk.Label(self, text="Please read the form below carefully.", font=Font(size=8, slant="italic")).pack()
-        frame = ScrollableFrame(self, scroll_y=True)
+        online_doc_url = "https://arielacil-my.sharepoint.com/personal/harelb_ariel_ac_il/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fharelb%5Fariel%5Fac%5Fil%2FDocuments%2FParticipant%20Information%20and%20Consent%20Form%2Epdf&parent=%2Fpersonal%2Fharelb%5Fariel%5Fac%5Fil%2FDocuments&ga=1"
+        ttk.Button(self, text="View Document Online", command=lambda: webbrowser.open(online_doc_url)).pack(anchor="e")
+
+        # scroll text
+        frame = ScrollableFrame(self, scroll_y=True, relief="sunken", padding=5)
         frame.pack(expand=True, fill='x')
         frame.canvas.config(width=600)
-        text_content = ttk.Label(frame.content, text=terms_text, anchor="nw", justify="left", relief="sunken")
+        text_content = ttk.Label(frame.content, text=terms_text, anchor="nw", justify="left")
         text_content.pack(expand=True, fill='x', padx=5, pady=5)
 
         def on_canvas_resize(event):
@@ -32,8 +37,8 @@ class TermsPage(WidgetFrame):
         b_frame = ttk.Frame(self)
         b_frame.pack()
         ttk.Label(self, text="")
-        ttk.Button(b_frame, text="Accept and Continue", command=lambda: self.on_accepted()).grid(row=0, column=0)
-        ttk.Button(b_frame, text="Reject", command=lambda: sys.exit()).grid(row=0, column=1)
+        ttk.Button(b_frame, text="Accept Terms and Continue", command=lambda: self.on_accepted()).grid(row=0, column=0)
+        # ttk.Button(b_frame, text="Reject", command=lambda: sys.exit()).grid(row=0, column=1)
 
 
 class AuthPage(WidgetFrame):

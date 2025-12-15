58198fea6b3e7f32eec170561611cd24585c0b20
ExoSkeleton <calazulazu@gmail.com>
2025-12-15 08:56:58 +0200

    terms/consent form

--- ui/auth.py
+++ ui/auth.py
@@ -1,23 +1,43 @@
+import sys
 from tkinter import ttk
+from tkinter.font import Font
 from typing import Callable, Any
 
 from ui.app import WidgetFrame
+from ui.scrollable_frame import ScrollableFrame
 
 
 class TermsPage(WidgetFrame):
     on_accepted: Callable[[], Any] = lambda b: None
 
     def _create_widgets(self):
-        ttk.Label(self, text="Terms and Conditions:").pack()
-        ttk.Label(
-            self, wraplength=400,
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pharetra augue id quam ullamcorper, ac dapibus augue elementum. Duis tempus pulvinar quam sed tristique. Nam placerat feugiat varius. Suspendisse ut nisl sit amet purus dignissim tristique sollicitudin fermentum libero. Nunc efficitur erat sed sapien feugiat faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur egestas iaculis..."
-        ).pack()
-        ttk.Button(self, text="Accept and Continue", command=lambda: self.on_accepted()).pack()
+        with open('terms.txt', 'r', encoding='utf-8') as f:
+            terms_text = f.read()
+
+        # terms
+        ttk.Label(self, text="Consent Form", font=Font(size=12, weight="bold")).pack()
+        ttk.Label(self, text="Please read the form below carefully.", font=Font(size=8, slant="italic")).pack()
+        frame = ScrollableFrame(self, scroll_y=True)
+        frame.pack(expand=True, fill='x')
+        frame.canvas.config(width=600)
+        text_content = ttk.Label(frame.content, text=terms_text, anchor="nw", justify="left", relief="sunken")
+        text_content.pack(expand=True, fill='x', padx=5, pady=5)
+
+        def on_canvas_resize(event):
+            text_content.configure(wraplength=event.width - 10)
+
+        frame.canvas.bind("<Configure>", on_canvas_resize)
+
+        # accept/reject buttons
+        b_frame = ttk.Frame(self)
+        b_frame.pack()
+        ttk.Label(self, text="")
+        ttk.Button(b_frame, text="Accept and Continue", command=lambda: self.on_accepted()).grid(row=0, column=0)
+        ttk.Button(b_frame, text="Reject", command=lambda: sys.exit()).grid(row=0, column=1)
 
 
 class AuthPage(WidgetFrame):
-    on_login: Callable[[str], Any] = lambda n: None
+    on_login: Callable[[str], Any] = lambda uuid: None
 
     def _create_widgets(self):
         ttk.Label(self, text="Login:").pack()

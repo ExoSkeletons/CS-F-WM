cf3d8d4b64e5d61f9443f891e7780dd2295afcdf
ExoSkeleton <calazulazu@gmail.com>
2025-12-15 08:55:13 +0200

    watermark thread, editable text ui setting, lang eng

--- ui/detect.py
+++ ui/detect.py
@@ -1,10 +1,12 @@
+import random
+import threading
 import tkinter
 from tkinter import END, font
 from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText
 from typing import Optional, Callable
 
-from ui.app import App, WidgetFrame, rtl, config_enable_frame
+from ui.app import App, WidgetFrame, config_enable_frame, set_text
 from ui.scrollable_frame import ScrollableFrame
 
 
@@ -17,8 +19,11 @@ class DetectPage(WidgetFrame):
 
     _response_cell: Optional[str] = None
 
-    def __init__(self, watermark: Callable[[str], str] | None, app: App, master: tkinter.Misc | None = None):
-        self.mark = watermark
+    def __init__(
+            self, app: App, master: tkinter.Misc | None = None,
+            watermark: Callable[[str], str] | None = None, mark_prob: float = 1.0,
+    ):
+        self.mark = random.choices([watermark, None], weights=[mark_prob, 1 - mark_prob])[0]
 
         super().__init__(app, master)
 
@@ -26,108 +31,118 @@ class DetectPage(WidgetFrame):
         # instructions
         ins_frame = ttk.Frame(self)
         wrap_l = 300
-        ins_frame.grid(row=0, column=1, sticky="nwe")
+        ins_col = 0
+        ins_frame.grid(row=0, column=ins_col, sticky="nwe")
+        self.question_frame = ttk.Frame(ins_frame)
+        self.question_frame.pack(expand=True, fill="both")
         ttk.Label(
-            ins_frame,
-            text=rtl(
-                "לפניכם שאלה לימודית.\nהשתמשו במודל שלפניכם וכתבו שאלה למודל - ארוכה ככל שתרצו, כדי לעזור לכם בפתרון השאלה."
-            ),
-            justify="right", anchor="ne", wraplength=wrap_l
+            self.question_frame,
+            text=
+            "You are given a question from a school assignment, and a Chat Model as an assistant.\n\n"
+            "Use the chat model for help with the assignment, by writing a single prompt question - as long as you'd like - to help you solve the question."
+            ,
+            justify="left", anchor="nw", wraplength=wrap_l
         ).pack(expand=True, fill="both")
-        ttk.Label(ins_frame, text=rtl("שאלה:")).pack()
+        ttk.Label(self.question_frame, text="question:").pack()
         ttk.Label(
-            ins_frame, text=rtl("< שאלה >"),
+            self.question_frame, text="< QUESTION >",
             font=font.Font(
                 size=self._font_size_title,
                 weight="bold", slant="italic"
             )
         ).pack()
 
-        model_frame = ttk.Frame(self)
-        model_frame.grid(row=0, column=0, sticky="nsew")
-        self.columnconfigure(0, weight=1)
-
+        model_frame = ttk.Frame(self, relief="sunken", padding=(5, 5))
+        model_col = 1
+        model_frame.grid(row=0, column=model_col, sticky="nsew")
+        self.columnconfigure(model_col, weight=1)
         # query label
         _user_font = font.Font(size=self._font_size, slant="italic")
         self.q_var: tkinter.StringVar = tkinter.StringVar()
         ttk.Label(model_frame, textvariable=self.q_var, font=_user_font).pack()
-
         # model response frame
-        self.scroll = ScrollableFrame(model_frame, scroll_y=True)
+        self.scroll = ScrollableFrame(model_frame, scroll_y=True, scroll_x=True)
         self.scroll.pack(fill="both", expand=True)
         self.text_var = tkinter.StringVar()
-        self.t = ttk.Label(self.scroll.content, textvariable=self.text_var, anchor="n")
-        self.bind("<Configure>", lambda event: self.t.configure(wraplength=event.width - 40))
-        self.t.pack(anchor="center")
+        # text form (editable)
+        self.tt = tkinter.Text(self.scroll.content, wrap="word")
+        self.text_var.trace_add("write", lambda var, index, mode: set_text(self.tt, self.text_var.get()))
+        self.tt.grid(row=0, column=0, sticky="nsew")
+        # text label (non-editable)
+        self.tl = tkinter.Label(self.scroll.content, textvariable=self.text_var, anchor="nw", justify="left")
+        self.tl.grid(row=0, column=0, sticky="nsew")
+        self.tl.bind("<Configure>", lambda event: self.tl.configure(wraplength=event.width - 50))
+        # query submission
+        self.submit_frame = ttk.Frame(model_frame)
+        self.submit_frame.pack(fill="x", expand=True)
+        self._query_form = ScrolledText(self.submit_frame, height=1, width=1, wrap="word")
+        self.app.set_on_submit(self._query_form, lambda: self.submit_query())
+        self._query_form.grid(row=0, column=0, sticky="nswe")
+        self.submit_frame.columnconfigure(0, weight=1)
+        query_button = ttk.Button(self.submit_frame, text="Send", command=lambda: self.submit_query())
+        query_button.grid(row=0, column=1)
+        self.app.set_on_submit(query_button, lambda: self.submit_query())
 
         # user response
         self.user_response_frame = ttk.Frame(ins_frame)
-        user_reasoning_frame = ttk.Frame(self.user_response_frame)
         self.is_watermarked_var = tkinter.BooleanVar()
         (ttk.Label(
             self.user_response_frame,
-            text=rtl("התבוננו בטקסט שלפניכם - האם לדעתכם מוסתר סימן מים בטקסט?"),
-            justify="right", wraplength=wrap_l
+            text="Observe the response text you've received:\ndo you believe it has been Watermarked?\n\nIf so, try to remove it by editing the text response.",
+            justify="left", wraplength=wrap_l
         ).grid(row=0, column=0))
         # checkbox
-        self.b = ttk.Checkbutton(self.user_response_frame, text="כן, יש סימן מים", variable=self.is_watermarked_var)
+        self.b = ttk.Checkbutton(self.user_response_frame, text="Yes, I see a Watermark",
+                                 variable=self.is_watermarked_var)
         self.b.grid(row=1, column=0)
         # reasoning
-        reasoning = ttk.Label(user_reasoning_frame, text=rtl("כיצד זיהיתם את הסימן?"))
+        user_reasoning_frame = ttk.Frame(self.user_response_frame)
         user_reasoning_frame.grid(row=2, column=0, sticky="nsew")
+        reasoning = ttk.Label(user_reasoning_frame, text="What made you think text was Watermarked?")
         self.reasoning_var = tkinter.StringVar()
-        reasoning.grid(row=0, column=1)
+        reasoning.grid(row=0, column=0)
         reasoning_entry = ttk.Entry(user_reasoning_frame, textvariable=self.reasoning_var)
-        reasoning_entry.grid(row=0, column=0, sticky="nsew")
-        user_reasoning_frame.columnconfigure(0, weight=1)
-        self.user_response_frame.columnconfigure(0, weight=1)
+        reasoning_entry.grid(row=1, column=0, sticky="nsew")
+        user_reasoning_frame.columnconfigure(1, weight=1)
+        self.user_response_frame.columnconfigure(1, weight=1)
+        # todo: instruct to try and remove the watermark
         # confirm
         answer_frame = ttk.Frame(self.user_response_frame)
         answer_frame.grid(row=3, column=0, columnspan=2)
         self._confirm_button = ttk.Button(answer_frame, text="Confirm Choice", command=lambda: self.submit_choices())
         self._confirm_button.grid(row=0, column=0)
-        self.response_correctness_var = tkinter.StringVar()
+        self._response_correctness_var = tkinter.StringVar()
         # results
-        ttk.Label(answer_frame, textvariable=self.response_correctness_var).grid(row=0, column=1)
+        ttk.Label(answer_frame, textvariable=self._response_correctness_var).grid(row=0, column=1)
+
+        def update_user_response(is_w: bool = False):
+            self.set_text_editable(is_w)
+            config_enable_frame(user_reasoning_frame, is_w)
 
         self.is_watermarked_var.trace_add(
             "write",
-            lambda var, index, mode: config_enable_frame(
-                user_reasoning_frame,
-                self.is_watermarked_var.get()
-            )
+            lambda var, index, mode: update_user_response(self.is_watermarked_var.get())
         )
         self.user_response_frame.pack(fill="x", expand=True)
         self.is_watermarked_var.set(False)
 
-        # query submission
-        self.submit_frame = ttk.Frame(model_frame)
-        self.submit_frame.pack(fill="x", expand=True)
-        self._text_form = ScrolledText(self.submit_frame, height=1, width=1, wrap="word")
-        self.app.set_on_submit(self._text_form, lambda: self.submit_query())
-        self._text_form.grid(row=0, column=0, sticky="nswe")
-        self.submit_frame.columnconfigure(0, weight=1)
-        query_button = ttk.Button(self.submit_frame, text="שלח", command=lambda: self.submit_query())
-        query_button.grid(row=0, column=1)
-        self.app.set_on_submit(query_button, lambda: self.submit_query())
-
         self.app.set_focus_next(reasoning_entry, self._confirm_button)
-        self.app.set_focus_next(self._text_form, self._confirm_button)
+        self.app.set_focus_next(self._query_form, self._confirm_button)
 
         super()._create_widgets()
 
-        self.response(None)
+        self.set_response_text("Hi! How can I help you today?")
 
     def submit_query(self):
         # get query
-        q = self._text_form.get('1.0', END)
+        q = self._query_form.get('1.0', END)
         if not q.strip(): return
 
         # set query label
         self.q_var.set(q)
 
         # clear responses
-        self.response("Generating Response...", apply_marks=False)
+        self.set_response_text("Generating Response...")
 
         # disable query form
         config_enable_frame(self.submit_frame, False)
@@ -136,41 +151,64 @@ class DetectPage(WidgetFrame):
         wrapped_q = q + "\n" + f"Your answer must be at least {self._min_word_count} words long."
         if self.on_submit: self.on_submit(wrapped_q)
 
-    def response(self, response: str | None, apply_marks=True):
-        # update model response text
-        is_response = response is not None
-        r = response if is_response else ""
-        w = self.mark
-        self.text_var.set(w(r) if w is not None and apply_marks else r)
-        self.b.config(state="normal" if is_response and apply_marks else "disabled")
-        self.is_watermarked_var.set(False)
+    def set_text_editable(self, enabled: bool = True):
+        if enabled:
+            self.tl.lower()
+            self.tt.lift()
+        else:
+            self.tl.lift()
+            self.tt.lower()
 
-        # update user response frame
-        if is_response and apply_marks:
+    def set_response_text(self, text: str | None, user_response_enabled: bool = False, re_query_enabled=True):
+        # update user instructions
+        if user_response_enabled:
+            self.question_frame.pack_forget()
             self.user_response_frame.pack()
         else:
+            self.question_frame.pack()
             self.user_response_frame.pack_forget()
 
-        self._confirm_button.config(state="normal" if is_response and apply_marks else "disabled")
-        self.response_correctness_var.set("")
+        # set text
+        self.text_var.set(text)
 
         # scroll to frame top bottom
         self.scroll.update_idletasks()
         self.scroll.canvas.yview_moveto(0.0)
 
         # re-enable query form
-        config_enable_frame(self.submit_frame, True)
+        config_enable_frame(self.submit_frame, re_query_enabled)
 
         # clear query form
-        self._text_form.delete('1.0', END)
+        self._query_form.delete('1.0', END)
+
+    def response(self, response: str | None, ok: bool = True):
+        # update model response text
+        if response is None or not ok:
+            self.app.after(0, lambda: self.set_response_text(response))
+            return
+
+        def watermark_worker():
+            wm = self.mark
+            wmr = wm(response) if wm is not None else response
+
+            self.is_watermarked_var.set(False)
+            self._response_correctness_var.set("")
+
+            # update UI safely from main thread
+            self.app.after(0, lambda: self.set_response_text(wmr, user_response_enabled=True, re_query_enabled=False))
+
+        self.set_response_text("Watermarking...")
+
+        threading.Thread(target=watermark_worker, daemon=True).start()
 
     def submit_choices(self):
         # lock in choices
         config_enable_frame(self, False)
+        self.set_text_editable(False)
 
         # mark correct choices
         w = self.mark
-        self.response_correctness_var.set(
+        self._response_correctness_var.set(
             "Correct!" if ((w is not None) == self.is_watermarked_var.get())
             else "Incorrect"
         )

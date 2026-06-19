e5c9455245c4d5dea8f4bb899241d97593d148de
ExoSkeleton <calazulazu@gmail.com>
2026-06-19 15:18:46 +0300

    add missing text value

--- web/server.py
+++ web/server.py
@@ -57,6 +57,8 @@ def watermark(data: dict, request: Request, response: Response):
     print(f"starting watermark job {job_id}")
     jobs[job_id] = {
         "job_id": job_id,
+        "wm": data["wm"],
+        "text": data["text"],
         "status": "pending",
         "result": None,
     }

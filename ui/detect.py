0b6cc0ee7e5161e6aaf528e9b714cb01fc3b0b6e
ExoSkeleton <calazulazu@gmail.com>
2026-07-08 21:01:38 +0300

    styled "no" in red

--- ui/detect.py
+++ ui/detect.py
@@ -207,7 +207,7 @@ class ResponseFrame(WidgetFrame):
         self.b_wm_no = ttk.Radiobutton(
             radio_frame, text="No",
             variable=self.is_wm_no_var,
-            bootstyle="success-outline-toolbutton",
+            bootstyle="danger-outline-toolbutton",
             command=lambda: self.is_wm_yes_var.set(not self.is_wm_no_var.get())
         )
         self.b_wm_yes.grid(row=0, column=0)

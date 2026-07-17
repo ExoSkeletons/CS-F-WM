f87ff7d28c8086d0a42c5996b695eb2415b93326
ExoSkeleton <calazulazu@gmail.com>
2026-07-17 10:48:18 +0300

    pad

--- ui/app.py
+++ ui/app.py
@@ -4,7 +4,7 @@ from typing import Union, Optional
 import ttkbootstrap as ttk
 
 from config import img_dir_path
-from ui.theme import pad_l, dim
+from ui.theme import pad_l, dim, padx
 
 
 # def rtl(text: str):
@@ -165,7 +165,7 @@ class HeaderWidget(WidgetFrame):
         self.logo_label = ttk.Label(self, image=self.logo_img)
         self.logo_label.grid(row=0, column=0, sticky="nsew")
         uni_logos_frame = ttk.Frame(self)
-        uni_logos_frame.grid(row=0, column=1, sticky="nsew")
+        uni_logos_frame.grid(row=0, column=1, sticky="nsew", padx=pad_l)
         self.logos_unis_labels = [
             ttk.Label(uni_logos_frame, image=img)
             for img in self.logos_uni_imgs
